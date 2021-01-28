d4661b17e2d6d6363b6f2e018122c81500535f6b
Lunush <76045303+lunush@users.noreply.github.com>
2021-01-28 10:14:49 +0300

    Add .prettierrc

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,40 +2,40 @@ import {
   ApolloClient,
   ApolloProvider,
   createHttpLink,
-  InMemoryCache,
-} from "@apollo/client";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
-import React from "react";
-import Home from "./components/Home";
-import Login from "./components/Login";
-import Register from "./components/Register";
-import Settings from "./components/Settings";
-import Profile from "./components/Profile";
-import Layout from "./layout/Layout";
-import { AuthProvider } from "./utils/auth";
-import { setContext } from "@apollo/client/link/context";
-import Post from "./components/Post";
-import { MenuProvider } from "react-native-popup-menu";
+  InMemoryCache
+} from '@apollo/client'
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
+import React from 'react'
+import Home from './components/Home'
+import Login from './components/Login'
+import Register from './components/Register'
+import Settings from './components/Settings'
+import Profile from './components/Profile'
+import Layout from './layout/Layout'
+import { AuthProvider } from './utils/auth'
+import { setContext } from '@apollo/client/link/context'
+import Post from './components/Post'
+import { MenuProvider } from 'react-native-popup-menu'
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:3456/graphql",
-  credentials: "include",
-});
+  uri: 'http://localhost:3456/graphql',
+  credentials: 'include'
+})
 
 const authLink = setContext((_, { headers }) => {
-  const token = sessionStorage.getItem("postyToken");
+  const token = sessionStorage.getItem('postyToken')
   return {
     headers: {
       ...headers,
-      Authorization: token ? `Bearer ${token}` : "",
-    },
-  };
-});
+      Authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
+  cache: new InMemoryCache()
+})
 
 const App: React.FC = () => {
   return (
@@ -57,7 +57,7 @@ const App: React.FC = () => {
         </MenuProvider>
       </ApolloProvider>
     </AuthProvider>
-  );
-};
+  )
+}
 
-export default App;
+export default App
